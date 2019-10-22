d9f51aa67d7ab614c5e9801a54fa2979d695e130
dlhansford <davhans@microsoft.com>
2019-10-22 13:49:56 -0500

    sad

---
+++ CopyArtifactsFolder.ps1
@@ -0,0 +1,13 @@
+﻿$storage_account = "https://sandboxstoragesthcntl.blob.core.windows.net/"
+
+$container_name = 'wvdtemplatefiles'  
+$blobs = Get-AzureStorageBlob -Container $container_name -Context $storage_account  
+
+$destination_path = 'C:\Artifacts'
+
+$blobs = Get-AzureStorageBlob -Container $container_name -Context $storage_account  
+foreach ($blob in $blobs)  
+{  
+   New-Item -ItemType Directory -Force -Path $destination_path  
+   Get-AzureStorageBlobContent -Container $container_name -Blob $blob.Name -Destination $destination_path -Context $storage_account  
+}  
